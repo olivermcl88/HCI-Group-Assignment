1805012cec3628e5f0ca163f5016b361d3ee3071
Keivan Shooshtarian <keivan.s10@gmail.com>
2025-12-02 00:08:47 +0000

    Created frontend of assignments page and drag & drop functionality. Addresses most of issue #6 and some of issue #8. Functionality with database yet to be done. Dummy data present for now.

---
+++ src/main/java/com/hci/ufosightings/web/AssignmentsController.java
@@ -0,0 +1,36 @@
+package com.hci.ufosightings.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.hci.ufosightings.service.AreaService;
+import com.hci.ufosightings.service.TeamService;
+import com.hci.ufosightings.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class AssignmentsController {
+
+    private final UserService userService;
+
+    private final AreaService areaService;
+
+    private final TeamService teamService;
+
+    // A simple controller to return "Hello, World!" message and lists
+    @GetMapping("assignments")
+    public String assignments(Model model) {
+        // call services and add lists to the model
+        model.addAttribute("users", userService.getAllUsers());
+        model.addAttribute("areas", areaService.getAllAreas());
+        model.addAttribute("teams", teamService.getAllTeams());
+
+        return "assignments";
+    }
+
+}
